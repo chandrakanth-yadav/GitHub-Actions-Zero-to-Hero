5d22a42e13fd5791480e889a3fc3779094c43b14
chandrakanth-yadav <chandu.yadav8989@gmail.com>
2024-08-30 00:20:46 -0500

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,13 @@
 # app.py
 # This is a test commit
+def sub(a, b):
 def add(a, b):
     return a + b
 
+def test_sub():
+    assert sub(2, 1) == 1
+    assert sub(1, 1) == 0
+    
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
